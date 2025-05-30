ab0e93bea8e2cf5f11c35a976a7d012ad3d89418
Pranav-sai02 <pranavsaikasinadhuni2@gmail.com>
2025-05-30 08:42:44 +0530

    initial commit

---
+++ src/main/java/com/neoteric/Employee.java
@@ -0,0 +1,21 @@
+package com.neoteric;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    private String name;
+
+    private double salary;
+
+    private String department;
+
+}
